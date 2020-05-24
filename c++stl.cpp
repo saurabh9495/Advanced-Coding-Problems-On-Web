bffc15b83e0d6aa866b9a880a63caf4bcc4774bd
saurabh9495 <saurabhkumar4829@gmail.com>
2020-05-24 22:26:01 +0530

    Coding Commits made on 24th May 2020

--- c++stl.cpp
+++ c++stl.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <set>
 using namespace std;
 
-int main()
+bool f(int x, int y)
+{
+    return x > y;
+}
+
+void vectorDemo()
 {
     //C++ stl
     vector<int> A = {11, 12, 13, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
@@ -27,9 +33,59 @@ int main()
     A.push_back(100);
     A.push_back(123);
     // 0 1 2 3 4 5 6 7 8 9 11 12 13 100 100 100 123
+
+    //1st way
     vector<int>::iterator fi = lower_bound(A.begin(), A.end(), 100);  //>=
     vector<int>::iterator fi2 = upper_bound(A.begin(), A.end(), 100); //>
 
     cout << *fi << " " << *fi2 << endl;
     cout << fi2 - fi << endl;
+
+    sort(A.begin(), A.end(), f);
+    vector<int>::iterator fi3;
+
+    // for (fi3 = A.begin(); fi3 != A.end(); fi3++)
+    // {
+    //     cout << *fi3 << endl;
+    // }
+
+    // 2nd way
+    vector<int> B = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    auto ii = lower_bound(B.begin(), B.end(), 16);
+    auto ii2 = upper_bound(B.begin(), B.end(), 19);
+    cout << *ii << " " << *ii2 << endl;
+    cout << *ii2 - *ii << endl;
+    sort(B.begin(), B.end(), f);
+
+    for (int x : B)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+    for (int &x : B)
+    {
+        x++;
+        cout << x << " ";
+    }
+    cout << endl;
+}
+
+void setDemo()
+{
+    set<int> s;
+    s.insert(24);
+    s.insert(30);
+    s.insert(-27);
+    for (int y : s)
+    {
+        cout << y << " ";
+    }
+}
+
+int main()
+{
+    //Vector Examples
+    // vectorDemo();
+    //Set Examples
+    setDemo();
 }
