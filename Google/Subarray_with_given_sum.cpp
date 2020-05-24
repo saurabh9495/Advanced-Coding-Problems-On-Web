55b7abcb2d27e21a735c14b81ff22cdc76dda36a
saurabh9495 <saurabhkumar4829@gmail.com>
2020-05-25 05:12:53 +0530

    Coding Commits made on 24th May 2020

---
+++ Google/Subarray_with_given_sum.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    for (int i = 0; i < test_cases; i++)
+    {
+        bool flag = true;
+        int noe, sum, target = 0;
+        cin >> noe >> sum;
+        vector<int> v(noe);
+        for (int i = 0; i < noe; i++)
+        {
+            cin >> v[i];
+        }
+        int j = 0, k = 0;
+        for (j = 0; j < noe; j++)
+        {
+            target += v[j];
+            if (target == sum)
+            {
+                cout << 1 << " " << j + 1 << endl;
+                flag = false;
+                break;
+            }
+            else if (target > sum)
+            {
+                break;
+            }
+        }
+        for (k = 0; k < j; k++)
+        {
+            target -= v[k];
+            if (target == sum)
+            {
+                cout << k + 2 << " " << j + 1 << endl;
+                flag = false;
+                break;
+            }
+        }
+        if (flag)
+        {
+            cout << -1 << endl;
+        }
+    }
+    return 0;
+}
